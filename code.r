ab6be05b0658a6cd19f8d48f96eafa2610a877dc
Anna Elisabeth Kazakova Lindegren <anna@kamishi.se>
2017-09-20 14:27:02 +0200

    Update code.r

--- code.r
+++ code.r
@@ -14,7 +14,7 @@ ggplot(data=clean_sthlm_temp, aes(year, avg_temp)) + geom_line() +
   xlab("") + ylab("Stockholm Average")
 
 #making a timeseries and smoothing average  of stockholm temperatures
-stockholm_ts<- ts(clean_sthlm_temp[,c("avg_temp")], frequency=12, start=c(1743,1))
+stockholm_ts<- ts(clean_sthlm_temp[,c("avg_temp")], frequency=1, start=c(1743,1))
 
 #plotting timeseries of stockholm temperatures
 plot.ts(stockholm_ts)
@@ -31,23 +31,18 @@ ggplot(data=clean_global_temp, aes(year, avg_temp)) + geom_line() +
   xlab("") + ylab("Global Average")
 
 #making a timeseries and smoothing average of global temperatures
-global_ts<- ts(clean_global_temp[,c("avg_temp")], frequency=12, start=c(1743,1))
+global_ts<- ts(clean_global_temp[,c("avg_temp")], frequency=1, start=c(1743,1))
 
 #plotting timeseries of global temperatures
 plot.ts(global_ts)
 smooth_plot_global<- SMA(global_ts,n=3)
 plot.ts(smooth_plot_global)
 
-#making one combined_ts from Stockholm and Global timeseries data (FROM HERE STARTS THE BOOBOO)
-combined_ts <-ts.union(stockholm_ts,global_ts, dframe=FALSE)
+#create df for colomn names
+data_names_legend<-data.frame("stockholm_ts","global_ts")
 
-#plottig combined_ts
-ts.plot(stockholm_ts, global_ts,
-        gpars=list(xlab="year", ylab="avg_temp", lty=c(1:3)))
-
-require(zoo) 
-plot(smooth_plot_global)
-lines(rollapply(smooth_plot_global, width = 10, FUN = mean), col = 'red')
-plot(smooth_plot_stockholm)
-lines(rollapply(smooth_plot_stockholm, width = 10, FUN = mean), col = 'blue')
+#plottig combined data of ts
+ts.plot(smooth_plot_stockholm, smooth_plot_global,
+        gpars=list(xlab="year", ylab="avg_temp", lty=c(1:3), col=1:ncol(data_names_legend)))
+legend("bottomright", legend = colnames(data_names_legend), col = 1:2, lty = 1, cex=.45)
 
