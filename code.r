f2e11438226bb730188d8d8230e7c240466e13b9
Anna Elisabeth Kazakova Lindegren <anna@kamishi.se>
2017-09-20 10:26:57 +0200

    Create code.r

---
+++ code.r
@@ -0,0 +1,39 @@
+library(ggplot2)
+library(TTR)
+
+stockholm_temperatures<-read.csv("/Users/batman/desktop/results_stockholm.csv")
+global_temperatures <-read.csv("/Users/batman/desktop/results_global.csv")
+
+#cleaning data frame with Stockholm average temperatures
+no_na_sthlm_tem <- na.omit(stockholm_temperatures)
+clean_sthlm_temp<- no_na_sthlm_tem[no_na_sthlm_tem$avg_temp > 0,]
+print(clean_sthlm_temp)
+
+#plotting cleaned Stockholm temperatures
+ggplot(data=clean_sthlm_temp, aes(year, avg_temp)) + geom_line() +  
+  xlab("") + ylab("Stockholm Average")
+
+#making a timeseries and smoothing average  of stockholm temperatures
+stockholm_ts<- ts(clean_sthlm_temp[,c("avg_temp")], frequency=12, start=c(1743,1))
+
+#plotting timeseries of stockholm temperatures
+plot.ts(stockholm_ts)
+smooth_plot_stockholm<- SMA(stockholm_ts,n=3)
+plot.ts(smooth_plot_stockholm)
+
+#cleaning data frame with Global average temperatures
+no_na_global_tem <- na.omit(global_temperatures)
+clean_global_temp<- no_na_global_tem[no_na_global_tem$avg_temp > 0,]
+print(clean_global_temp)
+
+#plotting cleaned Global temperatures
+ggplot(data=clean_global_temp, aes(year, avg_temp)) + geom_line() +  
+  xlab("") + ylab("Global Average")
+
+#making a timeseries and smoothing average of global temperatures
+global_ts<- ts(clean_global_temp[,c("avg_temp")], frequency=12, start=c(1743,1))
+
+#plotting timeseries of global temperatures
+plot.ts(global_ts)
+smooth_plot_global<- SMA(global_ts,n=3)
+plot.ts(smooth_plot_global)
